8c50d7d212c91d005d9743f2cc294d74c839e05f
Will C <wcarter312@gmail.com>
2018-08-15 20:31:34 -0500

    Addition of OSInit. Not currenlty 1:1, so will likely be revamping.

--- src/hsd/hsd_init.c
+++ src/hsd/hsd_init.c
@@ -40,6 +40,90 @@ void HSD_InitComponent(){
 	init_done = 1;
 }
 
+//80375304
+void HSD_OSInit(){
+	u8*    arenaLo;
+	u8*    arenaHi;
+    arenaLo = OSGetArenaLo();
+    arenaHi = OSGetArenaHi();
+	
+	if (!iparam_memory_callbacks) {
+		HSD_MemCallbacks cb;
+		cb.alloc_func = _HSD_MemAllocDefaultCB;
+		cb.free_func = _HSD_MemFreeDefaultCB;
+		cb.remain_func = _HSD_MemGetRemainDefaultCB;
+		cb.clear_func = _HSD_MemClearDefaultCB;
+		cb.own_func = _HSD_MemCheckOwnDefaultCB;
+		_HSD_MemSetCallbacks(&cb, sizeof(cb));
+		
+		if (hsd_heap_next_arena_lo && hsd_heap_next_arena_hi) {
+			hsd_heap_arena_lo = hsd_heap_next_arena_lo;
+			hsd_heap_arena_hi = hsd_heap_next_arena_hi;
+			hsd_heap_next_arena_lo = NULL;
+			hsd_heap_next_arena_hi = NULL;
+			
+			hsd_heap = OSCreateHeap(hsd_heap_arena_lo, hsd_heap_arena_hi);
+			OSSetCurrentHeap(hsd_heap);
+			HSD_ObjSetHeap((u32)hsd_heap_arena_hi - (u32)hsd_heap_arena_lo, NULL);
+		} else {
+		arenaLo = (void*)OSRoundUp32B(OSGetArenaLo());
+		arenaHi = OSGetArenaHi();
+		
+		hsd_heap_arena_lo = arenaLo;
+		if (iparam_heap_size > 0) {
+			hsd_heap_arena_hi = arenaLo + iparam_heap_size;
+			if (hsd_heap_arena_hi > arenaHi)
+				hsd_heap_arena_hi = arenaHi;
+			hsd_heap_arena_hi = (void*)OSRoundDown32B(hsd_heap_arena_hi);
+		} else {
+			hsd_heap_arena_hi = (void*)OSRoundDown32B(arenaHi);
+		}
+		hsd_heap = OSCreateHeap(hsd_heap_arena_lo, hsd_heap_arena_hi);
+		OSSetCurrentHeap(hsd_heap);
+		HSD_ObjSetHeap((u32)hsd_heap_arena_hi - (u32)hsd_heap_arena_lo, NULL);
+		OSSetArenaLo(hsd_heap_arena_hi);
+		}
+	}
+}
+
+static void* _HSD_MemAllocDefaultCB(u32 size, u32 align, u32 flags)
+{
+#pragma unused(align, flags)
+  void *addr;
+
+  if (size == 0)
+    return NULL;
+
+  addr = OSAllocFromHeap(HSD_GetHeap(), size);
+  assert (addr);
+  return addr;
+} 
+
+static void _HSD_MemFreeDefaultCB(void *addr){
+	OSFreeToHeap(HSD_GetHeap(), addr);
+}
+
+static u32 _HSD_MemGetRemainDefaultCB(void){
+	return OSCheckHeap(HSD_GetHeap());
+}
+
+static void _HSD_MemClearDefaultCB(void)
+{
+  OSDestroyHeap(hsd_heap);
+  if (hsd_heap_next_arena_lo && hsd_heap_next_arena_hi) {
+    hsd_heap_arena_lo = hsd_heap_next_arena_lo;
+    hsd_heap_arena_hi = hsd_heap_next_arena_hi;
+    hsd_heap_next_arena_lo = NULL;
+    hsd_heap_next_arena_hi = NULL;
+  }
+  hsd_heap = OSCreateHeap(hsd_heap_arena_lo, hsd_heap_arena_hi);
+  OSSetCurrentHeap(hsd_heap);
+}
+
+static int _HSD_MemCheckOwnDefaultCB(void *addr){
+	return hsd_heap_arena_lo <= addr && addr < hsd_heap_arena_hi;
+}
+
 //803755B4
 void HSD_ObjInit(){
   HSD_ListInitAllocData();
