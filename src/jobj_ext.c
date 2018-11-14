21c671e66cd282421748f17bfd18098baa169f8d
Will C <wcarter312@gmail.com>
2018-11-13 19:53:21 -0600

    Correct a mistake in the JObj extension

--- src/jobj_ext.c
+++ src/jobj_ext.c
@@ -1,9 +1,17 @@
 #include "jobj_ext.h"
 
-//8000C228
+//8000C1C0
 void JObj_AttachJoint_CopyPos(HSD_JObj* jobj_attach, HSD_JObj* jobj_bone){
     HSD_RObj* robj = HSD_RObjAlloc();
-    HSD_RObjSetFlags(robj, 0x90000004);
+    HSD_RObjSetFlags(robj, 0x90000001);
+    HSD_RObjSetConstraintObj(robj, jobj_bone);
+    HSD_JObjPrependRObj(jobj_attach, robj);
+}
+
+//8000C228
+void JObj_AttachJoint_CopyUnk(HSD_JObj* jobj_attach, HSD_JObj* jobj_bone){
+    HSD_RObj* robj = HSD_RObjAlloc();
+    HSD_RObjSetFlags(robj, 0x90000002);
     HSD_RObjSetConstraintObj(robj, jobj_bone);
     HSD_JObjPrependRObj(jobj_attach, robj);
 }
