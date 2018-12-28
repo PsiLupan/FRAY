ccb00f695a5f41226c07dbcaf9cd12e8e6a4758e
Will C <wcarter312@gmail.com>
2018-12-28 16:46:00 -0600

    Add Capt. Falcon's OnLoad function.

---
+++ src/ftCFalcon.c
@@ -0,0 +1,20 @@
+#include "ftCFalcon.h"
+
+//800E2AEC
+void CFalcon_OnLoad(HSD_GObj* gobj){
+    Player* player = GOBJ_PLAYER(gobj);
+    player->x2224_flags = player->x2224_flags & 0xFE | 1;
+    u8* char_dat = player->x10C_char_dat;
+    f32* player_afp = player->x2D8_player_article_floats;
+    u32 i = 17;
+    do {
+        f32 data1 = ((f32*)char_dat)[2];
+        char_dat = char_dat + 8; //Update offset by 8 bytes, so 4 bytes are skipped between each AFP
+        f32 data2 =  ((f32*)char_dat)[1];
+        player_afp[2] = data1;
+        player_afp = player_afp + 8;
+        player_afp[1] = data2;
+        --i;
+    } while(i > 0);
+    player->x2D4_player_article_floats = player->x2D8_player_article_floats;
+}
---
+++ src/ftCFalcon.h
@@ -0,0 +1,13 @@
+#ifndef _ftCFalcon_h_
+#define _ftCFalcon_h_
+
+#include <gctypes.h>
+
+#include "hsd/hsd_jobj.h"
+
+#include "gobj.h"
+#include "interrupt.h"
+#include "item.h"
+#include "player.h"
+
+#endif
