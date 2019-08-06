a01a4fc51c566a6b4191fe7aeb6f4abce46336ae
Will C <wcarter312@gmail.com>
2019-08-06 00:28:13 -0500

    Set explicit conversion in TObjRemoveAll

--- src/hsd/hsd_tobj.c
+++ src/hsd/hsd_tobj.c
@@ -1317,8 +1317,8 @@ u32 HSD_TexMap2Index(u8 mapid){
 void HSD_TObjRemoveAll(HSD_TObj *tobj){
 	while (tobj) {
 		HSD_TObj *next = tobj->next;
-		HSD_CLASS_METHOD(tobj)->release(tobj);
-		HSD_CLASS_METHOD(tobj)->destroy(tobj);
+		HSD_CLASS_METHOD(tobj)->release((HSD_Class*)tobj);
+		HSD_CLASS_METHOD(tobj)->destroy((HSD_Class*)tobj);
 		tobj = next;
 	}
 }
