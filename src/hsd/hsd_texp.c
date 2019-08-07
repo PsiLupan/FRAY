b7179420e615395f8fac046559ee821fff6a440e
Will C <wcarter312@gmail.com>
2019-08-07 00:42:41 -0500

    Implement several TExp functions and setup placeholders.

--- src/hsd/hsd_texp.c
+++ src/hsd/hsd_texp.c
@@ -5,10 +5,40 @@
 
 static u32 num_texgens = 0; //r13_40A4
 
+//80362478
+void HSD_StateRegisterTexGen(u32 coord){
+    u32 num = HSD_TexCoordID2Num(coord);
+    if(num_texgens < num){
+        num_texgens = num;
+    }
+}
+
 //803624A8
 void HSD_StateSetNumTexGens(){
     GX_SetNumTexGens(num_texgens);
     num_texgens = 0;
+}
+
+//803629D8
+void HSD_SetTevRegAll(){
+
+}
+
+//80362AA4
+u32 HSD_TexCoordID2Num(u32 coord){
+    switch(coord){
+        case GX_TEXCOORD0: return 1;
+        case GX_TEXCOORD1: return 2;
+        case GX_TEXCOORD2: return 3;
+        case GX_TEXCOORD3: return 4;
+        case GX_TEXCOORD4: return 5;
+        case GX_TEXCOORD5: return 6;
+        case GX_TEXCOORD6: return 7;
+        case GX_TEXCOORD7: return 8;
+        case GX_TEXCOORDNULL: return 0;
+        default: assert(0);
+    }
+    return 0;
 }
 
 //80382C00
@@ -106,6 +136,53 @@ HSD_TExp* HSD_TExpTev(HSD_TExp** list){
 //803831BC
 HSD_TExp* HSD_TExpCnst(void* color, u32 value, u32 type, HSD_TExp** list){
 
+}
+
+//803832D0
+void HSD_TExpColorOp(HSD_TExp* texp, u8 op, u8 bias, u8 scale, u8 enable){
+    assert(HSD_TExpGetType(texp) == 1);
+    texp->color_op = op;
+    texp->color_enable = (enable ? GX_ENABLE : GX_DISABLE);
+    if(op < 2){
+        texp->color_bias = bias;
+        texp->color_scale = scale;
+    }
+    texp->color_bias = 0;
+    texp->color_scale = 0;
+}
+
+//803833AC
+void HSD_TExpAlphaOp(HSD_TExp* texp, u8 op, u8 bias, u8 scale, u8 enable){
+    assert(HSD_TExpGetType(texp) == 1);
+    texp->alpha_op = op;
+    texp->alpha_enable = (enable ? GX_ENABLE : GX_DISABLE);
+    if(op < 2){
+        texp->alpha_bias = bias;
+        texp->alpha_scale = scale;
+    }
+    texp->alpha_bias = 0;
+    texp->alpha_scale = 0;
+}
+
+//80383488
+static void HSD_TExpColorInSub(HSD_TExp* texp, u8 te_num, u8 texp_num, u32 sel){
+
+}
+
+//80383A64
+void HSD_TExpColorIn(HSD_TExp* texp, u8 te_num, u8 texp_num, u8 te_num2, u8 texp_num2, 
+    u8 te_num3, u8 texp_num3, u8 type, HSD_TExp* texp_rgb){
+    assert(HSD_TExpGetType(texp) == 1);
+    HSD_TExpColorInSub(texp, te_num, texp_num, 0);
+    HSD_TExpColorInSub(texp, te_num2, texp_num2, 1);
+    HSD_TExpColorInSub(texp, te_num3, texp_num3, 2)
+    HSD_TExpColorInSub(texp, type, texp_rgb, 3);
+}
+
+//80383F50
+void HSD_TExpAlphaIn(HSD_TExp* texp, u8 te_num, u8 texp_num, u8 te_num2, u8 texp_num2, 
+    u8 te_num3, u8 texp_num3, u8 type, HSD_TExp* texp_alpha){
+    assert(HSD_TExpGetType(texp) == 1);
 }
 
 //80384050
