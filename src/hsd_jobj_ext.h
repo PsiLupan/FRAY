c525cb52d343335360364931f88f6c0c33ed9bf0
William Carter <wcarter312@gmail.com>
2019-08-09 13:59:47 -0500

    Remove SetupInstanceMtx proto from Ext header

--- src/hsd_jobj_ext.h
+++ src/hsd_jobj_ext.h
@@ -13,7 +13,6 @@
 void JObj_Spawn(HSD_JObj*, guVector*, guVector*);
 void JObj_SetAnimSpeed(HSD_JObj*, f32[3]);
 f32 JObj_GetFrame(HSD_JObj*);
-void JObj_SetupInstanceMtx(HSD_JObj*, MtxP, u32, u32);
 void JObj_SetupInstanceMtx_Callback(HSD_GObj*, u32);
 
 #endif
