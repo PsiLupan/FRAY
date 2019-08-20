140ae51328968a83c29905b58bb168267cc68be1
William Carter <wcarter312@gmail.com>
2019-08-20 12:27:57 -0500

    Correct address for HSD_ArchiveParse

--- src/hsd/hsd_archive.c
+++ src/hsd/hsd_archive.c
@@ -2,6 +2,51 @@
 
 #include <string.h>
 
+
+//803801E4
+s32 HSD_ArchiveParse(HSD_Archive* archive, u8* src, u32 file_size)
+{
+    if (archive == NULL) {
+        return -1;
+    } else {
+        memset(archive, 0, sizeof(HSD_Archive));
+        archive->flags |= 1;
+        memcpy(archive, src, sizeof(HSD_ArchiveHeader));
+        if (archive->header.file_size == file_size) {
+            u32 offset = sizeof(HSD_ArchiveHeader);
+            if (archive->header.data_size != 0) { //Body Size
+                archive->data = src + 0x20;
+                offset = archive->header.data_size + 0x20;
+            }
+            if (archive->header.nb_reloc != 0) { //Relocation Size
+                archive->reloc_info = (HSD_ArchiveRelocationInfo*)((s32)src + offset);
+                offset = offset + archive->header.nb_reloc * 4;
+            }
+            if (archive->header.nb_public != 0) { //Root Size
+                archive->public_info = (HSD_ArchivePublicInfo*)((s32)src + offset);
+                offset = offset + archive->header.nb_public * 8;
+            }
+            if (archive->header.nb_extern != 0) { //XRef Size
+                archive->extern_info = (HSD_ArchiveExternInfo*)((s32)src + offset);
+                offset = offset + archive->header.nb_extern * 8;
+            }
+            if (offset < archive->header.file_size) { //File Size
+                archive->symbols = (char*)((s32)src + offset);
+            }
+            archive->top_ptr = (void*)src;
+            for (u32 i = 0; i < archive->header.nb_reloc; i++) {
+                u8* data = archive->data;
+                u32 offset = archive->reloc_info[i].offset;
+                *(u8**)(data + offset) = data + *((u32*)(data + offset));
+            }
+        } else {
+            HSD_Panic("Archive_Parse: Byte-Order mismatch");
+            return -1;
+        }
+    }
+    return 0;
+}
+
 //80380358
 void* HSD_ArchiveGetPublicAddress(HSD_Archive* archive, char* symbols)
 {
@@ -57,48 +102,4 @@ void HSD_ArchiveLocateExtern(HSD_Archive* archive, char* symbols, void* addr)
         offset += 8;
         ++xref;
     } while (true);
-}
-
-//803810E4
-s32 HSD_ArchiveParse(HSD_Archive* archive, u8* src, u32 file_size)
-{
-    if (archive == NULL) {
-        return -1;
-    } else {
-        memset(archive, 0, sizeof(HSD_Archive));
-        archive->flags |= 1;
-        memcpy(archive, src, sizeof(HSD_ArchiveHeader));
-        if (archive->header.file_size == file_size) {
-            u32 offset = sizeof(HSD_ArchiveHeader);
-            if (archive->header.data_size != 0) { //Body Size
-                archive->data = src + 0x20;
-                offset = archive->header.data_size + 0x20;
-            }
-            if (archive->header.nb_reloc != 0) { //Relocation Size
-                archive->reloc_info = (HSD_ArchiveRelocationInfo*)((s32)src + offset);
-                offset = offset + archive->header.nb_reloc * 4;
-            }
-            if (archive->header.nb_public != 0) { //Root Size
-                archive->public_info = (HSD_ArchivePublicInfo*)((s32)src + offset);
-                offset = offset + archive->header.nb_public * 8;
-            }
-            if (archive->header.nb_extern != 0) { //XRef Size
-                archive->extern_info = (HSD_ArchiveExternInfo*)((s32)src + offset);
-                offset = offset + archive->header.nb_extern * 8;
-            }
-            if (offset < archive->header.file_size) { //File Size
-                archive->symbols = (char*)((s32)src + offset);
-            }
-            archive->top_ptr = (void*)src;
-            for (u32 i = 0; i < archive->header.nb_reloc; i++) {
-                u8* data = archive->data;
-                u32 offset = archive->reloc_info[i].offset;
-                *(u8**)(data + offset) = data + *((u32*)(data + offset));
-            }
-        } else {
-            HSD_Panic("Archive_Parse: Byte-Order mismatch");
-            return -1;
-        }
-    }
-    return 0;
 }
