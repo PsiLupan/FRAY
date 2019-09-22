e82813a525081c60c68eee847145f2ed0ea07ead
William Carter <wcarter312@gmail.com>
2019-09-22 03:45:46 -0500

    RObj struct corrections

--- src/hsd/hsd_robj.h
+++ src/hsd/hsd_robj.h
@@ -9,45 +9,78 @@
 #define TYPE_MASK 0x70000000
 #define REFTYPE_JOBJ 0x10000000
 
+typedef struct _HSD_Rvalue {
+    struct _HSD_Rvalue* next;
+    u32 flags;
+    struct _HSD_JObj* jobj;
+} HSD_Rvalue;
+
+typedef struct _HSD_RvalueList {
+    u32 flags;
+    struct _HSD_JObjDesc* joint;
+    struct _HSD_RvalueList* next;
+} HSD_RvalueList;
+
+typedef struct _HSD_IKHint {
+    f32 bone_length;
+    f32 rotate_x;
+} HSD_IKHint;
+
+typedef struct _HSD_IKHintDesc {
+    f32 bone_length;
+    f32 rotate_x;
+} HSD_IKHintDesc;
+
+typedef struct _HSD_Exp {
+    union {
+        f32 (*func)(void);
+        u8* bytecode;
+    } expr;
+    struct _HSD_Rvalue* rvalue;
+    u32 nb_args;
+    u8 is_bytecode;
+} HSD_Exp;
+
+typedef struct _HSD_ExpDesc {
+    f32 (*func)(void);
+    struct _HSD_RvalueList* rvalue;
+} HSD_ExpDesc;
+
+typedef struct _HSD_ByteCodeExpDesc {
+    u8* bytecode;
+    struct _HSD_RvalueList* rvalue;
+} HSD_ByteCodeExpDesc;
+
 typedef struct _HSD_RObj {
     struct _HSD_RObj* next; //0x00
     u32 flags; //0x04
     union {
-        HSD_SList* rvalue;
-        f32 fv;
         struct _HSD_JObj* jobj;
+        struct _HSD_Exp exp;
+        f32 limit;
+        struct _HSD_IKHint ik_hint;
     } u; //0x08
     guVector pos; //0xC
     struct _HSD_AObj* aobj; //0x18
 } HSD_RObj;
 
-typedef struct _HSD_Rvalue {
-    struct _HSD_Rvalue* next; //0x00
-    u32 x4_unk;
-    struct _HSD_JObj* jobj;
-} HSD_Rvalue;
-
-typedef struct _HSD_RvalueDesc {
-    u32 unk;
-    struct _HSD_JObjDesc* jobjdesc; //0x04
-    struct _HSD_RvalueDesc* next; //0x08
-} HSD_RvalueDesc;
-
 typedef struct _HSD_RObjDesc {
     struct _HSD_RObjDesc* next; //0x00
     u32 flags; //0x04
     union {
-        HSD_SList* rvalue;
-        f32 fv;
-        struct _HSD_JObjDesc* jobjdesc;
+        u32 i;
+        struct _HSD_ExpDesc* exp;
+        struct _HSD_ByteCodeExpDesc* bcexp;
+        struct _HSD_IKHintDesc* ik_hint;
+        struct _HSD_JObjDesc* joint;
+        f32 limit;
     } u; //0x08
-    guVector pos; //0xC
 } HSD_RObjDesc;
 
-typedef struct _HSD_RObjAnim {
-    struct _HSD_RObjAnim* next;
+typedef struct _HSD_RObjAnimJoint {
+    struct _HSD_RObjAnimJoint* next;
     struct _HSD_AObjDesc* aobjdesc;
-} HSD_RObjAnim;
+} HSD_RObjAnimJoint;
 
 void HSD_RObjInitAllocData(void);
 HSD_ObjAllocData* HSD_RObjGetAllocData(void);
@@ -59,7 +92,7 @@ void HSD_RObjRemoveAnimAllByFlags(HSD_RObj*, u32);
 void HSD_RObjRemoveAnimAll(HSD_RObj*);
 void HSD_RObjReqAnimAllByFlags(HSD_RObj*, u32, f32);
 void HSD_RObjReqAnimAll(HSD_RObj*, f32);
-void HSD_RObjAddAnimAll(HSD_RObj*, HSD_RObjAnim*);
+void HSD_RObjAddAnimAll(HSD_RObj*, HSD_RObjAnimJoint*);
 u32 HSD_RObjGetGlobalPosition(HSD_RObj*, u32, guVector*);
 void HSD_RObjUpdateAll(HSD_RObj*, void*, void (*)(void*, u32, update*));
 void HSD_RObjResolveRefsAll(HSD_RObj*, HSD_RObjDesc*);
@@ -68,6 +101,6 @@ void HSD_RObjRemove(HSD_RObj*);
 void HSD_RObjRemoveAll(HSD_RObj*);
 HSD_RObj* HSD_RObjAlloc(void);
 void HSD_RObjFree(HSD_RObj*);
-void HSD_RvalueResolveRefsAll(HSD_Rvalue*, HSD_RvalueDesc*);
+void HSD_RvalueResolveRefsAll(HSD_Rvalue*, HSD_RvalueList*);
 
 #endif
