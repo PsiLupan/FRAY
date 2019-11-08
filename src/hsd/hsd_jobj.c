c9d57c30f8cf1ddd9996c194f7f9dfc518af5ba3
Will C <wcarter312@gmail.com>
2019-11-07 20:52:01 -0600

    Remove erroneous semicolon

--- src/hsd/hsd_jobj.c
+++ src/hsd/hsd_jobj.c
@@ -771,7 +771,6 @@ static void JObjAnimAll(HSD_JObj* jobj)
         HSD_JObjAnim(jobj);
         if (JOBJ_INSTANCE(jobj)) {
             for (HSD_JObj* i = jobj->child; i != NULL; i = i->next) {
-                ;
                 HSD_JObjAnim(i);
                 if (JOBJ_INSTANCE(i)) {
                     for (HSD_JObj* j = i->child; j != NULL; j = j->next) {
