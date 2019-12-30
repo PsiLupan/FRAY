1ea52d7ca1ac235c3d0bacffe15aebef47de7811
Will C <wcarter312@gmail.com>
2019-12-30 15:37:18 -0600

    Variable cleanup

--- src/util.c
+++ src/util.c
@@ -6,7 +6,7 @@ void stub()
     return;
 }
 
-u64 concat_to_64(u32 x, u32 y)
+/*u64 concat_to_64(u32 x, u32 y)
 {
     u64 pow = 10;
     while (y >= pow)
@@ -17,4 +17,4 @@ u64 concat_to_64(u32 x, u32 y)
 u64 __shr2u(u32 x, u32 y, s32 shift)
 {
     return concat_to_64(x >> shift, y >> shift | x << (0x20 - shift) | x >> (shift + -0x20));
-}
+}*/
