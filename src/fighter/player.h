d60338092ae7a0a1c7a7bea17614ac8ca3bfee7a
Will C <wcarter312@gmail.com>
2020-06-19 14:48:27 -0500

    Add new collision check and move physics to fighter directory

--- src/fighter/player.h
+++ src/fighter/player.h
@@ -8,6 +8,8 @@
 
 #include "hsd/hsd_gobj.h"
 
+#include "physics.h"
+
 #include "subaction.h"
 
 #define GOBJ_PLAYER(s) ((Player*)s->data)
@@ -154,20 +156,6 @@ typedef struct _Attributes {
     u32 specialJumpAction; //0x17C - Pretty much an unknown, need to research what uses this
     u32 throwSpeedFlags; //0x180
 } Attributes;
-
-typedef struct _Physics {
-    void *unk;
-    guVector x4_xC_TopN;
-    guVector x10_x18_TopN_copy; //Assumed
-    guVector x1C_x24_TopN_prev;
-    guVector x28_x30_TopN_prev_copy; //Assumed
-    
-    s32 x3C_floor_id_actionstate;
-    
-    u32 x130_unk;
-    s32 x14C_floor_id;
-    u32 x150_ground_type;
-} Physics;
 
 typedef struct _Player {
     struct _HSD_GObj* parent;
